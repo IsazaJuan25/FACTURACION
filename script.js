e33ce8222a1f871651ffcaa9026829c169b4d2c0
IsazaJuan <juanisaza219@gmail.com>
2025-08-16 17:51:24 -0500

    Función para calcular el consumo mensual agregada

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 console.log("==== FACTURA DE SERVICIO ELÉCTRICO ====");
 console.log("Cliente: Juan Pérez");
 
+function calcularConsumoMensual() {
+    let kilovatiosConsumidos = prompt("¿Cuántos kilovatios se consumieron este mes?");
+    const diasDelMes = prompt("¿Cuántos días tiene el mes?");
+    return kilovatiosConsumidos * diasDelMes;
+}
+
+let consumoDelMes = calcularConsumoMensual();
